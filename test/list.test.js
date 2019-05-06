b14f76332afc2d55d25dde954c52ab6ffdf9d23a
Bryan Terce <bryan.terce@gmail.com>
2019-05-06 10:38:15 -0700

    Add more tests to list

--- test/list.test.js
+++ test/list.test.js
@@ -15,6 +15,20 @@ test("list properly formats arrays", () => {
   expect(fnt`${listOr()}`(arr)).toBe("foo, bar, or baz")
 })
 
+test("list properly formats arrays with 1 element", () => {
+  const arr = ["foo"]
+  expect(fnt`${list()}`(arr)).toBe("foo")
+  expect(fnt`${listAnd()}`(arr)).toBe("foo")
+  expect(fnt`${listOr()}`(arr)).toBe("foo")
+})
+
+test("list properly formats arrays with no elements", () => {
+  const arr = []
+  expect(fnt`${list()}`(arr)).toBe("")
+  expect(fnt`${listAnd()}`(arr)).toBe("")
+  expect(fnt`${listOr()}`(arr)).toBe("")
+})
+
 test("list uses get when a key is passed", () => {
   const context = {
     key: ["foo", "bar", "baz"]
