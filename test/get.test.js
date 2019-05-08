968ae00735494f4f1fe51c2ce89d7b754268bf5b
Bryan Terce <bryan.terce@gmail.com>
2019-05-08 12:53:09 -0700

    Isolate get tests

--- test/get.test.js
+++ test/get.test.js
@@ -1,24 +1,25 @@
 const { fnt, get } = require("../dist/fntstring")
 
 test("get evaluates to context[key]", () => {
-  expect(fnt`${get("foo")}`({ foo: "test" })).toBe("test")
-  expect(fnt`${get(0)}`(["test"])).toBe("test")
+  expect(get("foo")({ foo: "test" })).toBe("test")
+  expect(get(0)(["test"])).toBe("test")
 })
 
 test("get evaluates nested properties", () => {
-  expect(fnt`${get("foo.bar")}`({ foo: { bar: "test" } })).toBe("test")
+  expect(get("foo.bar")({ foo: { bar: "test" } })).toBe("test")
+  expect(get("foo.0")({ foo: ["test"] })).toBe("test")
 })
 
 test("get returns context when no key is passed", () => {
-  expect(fnt`${get()}`("test")).toBe("test")
-  expect(fnt`${get(undefined)}`("test")).toBe("test")
+  expect(get()("test")).toBe("test")
+  expect(get(undefined)("test")).toBe("test")
 })
 
 test("get errors when the key does not exist", () => {
   expect(() => {
-    fnt`${get(null)}`("test")
+    get(null)("test")
   }).toThrow()
   expect(() => {
-    fnt`${get("test")}`("test")
+    get("test")("test")
   }).toThrow()
 })
