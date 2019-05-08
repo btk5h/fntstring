f619b21efb6f1c8262836211772e056fe9826b79
Bryan Terce <bryan.terce@gmail.com>
2019-05-08 13:00:39 -0700

    Isolate list tests

--- test/list.test.js
+++ test/list.test.js
@@ -2,32 +2,32 @@ const { fnt, list, listAnd, listOr, FntMeta } = require("../dist/fntstring")
 
 test("list errors with non-array parameters", () => {
   expect(() => {
-    fnt`${list()}`()
+    list()()
   }).toThrow()
   expect(() => {
-    fnt`${list("foo")}`({ foo: "test" })
+    list("foo")({ foo: "test" })
   }).toThrow()
 })
 
 test("list properly formats arrays", () => {
   const arr = ["foo", "bar", "baz"]
-  expect(fnt`${list()}`(arr)).toBe("foo, bar, baz")
-  expect(fnt`${listAnd()}`(arr)).toBe("foo, bar, and baz")
-  expect(fnt`${listOr()}`(arr)).toBe("foo, bar, or baz")
+  expect(list()(arr)).toBe("foo, bar, baz")
+  expect(listAnd()(arr)).toBe("foo, bar, and baz")
+  expect(listOr()(arr)).toBe("foo, bar, or baz")
 })
 
 test("list properly formats arrays with 1 element", () => {
   const arr = ["foo"]
-  expect(fnt`${list()}`(arr)).toBe("foo")
-  expect(fnt`${listAnd()}`(arr)).toBe("foo")
-  expect(fnt`${listOr()}`(arr)).toBe("foo")
+  expect(list()(arr)).toBe("foo")
+  expect(listAnd()(arr)).toBe("foo")
+  expect(listOr()(arr)).toBe("foo")
 })
 
 test("list properly formats arrays with no elements", () => {
   const arr = []
-  expect(fnt`${list()}`(arr)).toBe("")
-  expect(fnt`${listAnd()}`(arr)).toBe("")
-  expect(fnt`${listOr()}`(arr)).toBe("")
+  expect(list()(arr)).toBe("")
+  expect(listAnd()(arr)).toBe("")
+  expect(listOr()(arr)).toBe("")
 })
 
 test("list uses get when a key is passed", () => {
@@ -35,7 +35,7 @@ test("list uses get when a key is passed", () => {
     key: ["foo", "bar", "baz"]
   }
 
-  expect(fnt`${list("key")}`(context)).toBe("foo, bar, baz")
-  expect(fnt`${listAnd("key")}`(context)).toBe("foo, bar, and baz")
-  expect(fnt`${listOr("key")}`(context)).toBe("foo, bar, or baz")
+  expect(list("key")(context)).toBe("foo, bar, baz")
+  expect(listAnd("key")(context)).toBe("foo, bar, and baz")
+  expect(listOr("key")(context)).toBe("foo, bar, or baz")
 })
